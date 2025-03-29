ff36b34fe7fd97a1aba90ed4225a95b68f0b779c
Omniscient <omniscient.oce@gmail.com>
2025-03-29 21:10:11 +1100

    add a icons module

--- lib/ui/ctx_sidebar.ml
+++ lib/ui/ctx_sidebar.ml
@@ -12,20 +12,20 @@ type sidebar = [
 type nav_item = {
   text: string;
   href: string;
-  icon: string option;
+  icon: Icons.Octicons.t option;
 }
 
 let nav_items (sbar: sidebar) : nav_item list = let open Printf in
   match sbar with
   | `Home -> [
-      { text = "Repos"; href = "/"; icon = None };
+      { text = "Repos"; href = "/"; icon = Some Icons.Octicons.Repo };
       { text = "About"; href = "" (* TODO: customisable About (company, person, instance) page *); icon = None };
       { text = "Latest Activity"; href = "" (* TODO *); icon = None }
     ]
   | `Repo r -> 
       let after_repo_url = (fun s -> sprintf "/%s/%s" r.name s ) in
       [
-      { text = "Branches";  href = after_repo_url "branches"; icon = None };
+      { text = "Branches";  href = after_repo_url "branches"; icon = Some Icons.Octicons.Branch };
       { text = "Commits";   href = after_repo_url "commits";  icon = None };
       { text = "Tags";      href = after_repo_url "tags";     icon = None }
       ]
